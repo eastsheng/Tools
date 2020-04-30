5aaad5839a381174fc9359cc72178115a8821679
eastsheng <eastsheng@hotmail.com>
2020-04-30 19:29:26 +0800

    Create setup.py

---
+++ tools/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+
+	name         = 'tools',
+	version      = '1.1.0',
+	py_modules   = ['tools'],
+	author       = 'DongshengChen',
+	author_email = 'eastsheng@hotmail.com',
+	url          = 'https://github.com/eastsheng',
+	description  = 'Some tools.'
+	)
