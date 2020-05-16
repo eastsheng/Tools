0429f97501723668ed5a75734959bfb0c18b221c
eastsheng <eastsheng@hotmail.com>
2020-05-16 22:24:44 +0800

    Update setup.py

--- tools/setup.py
+++ tools/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
 
 	name         = 'tools',
-	version      = '1.1.1',
+	version      = '1.1.2',
 	py_modules   = ['tools'],
 	author       = 'DongshengChen',
 	author_email = 'eastsheng@hotmail.com',
