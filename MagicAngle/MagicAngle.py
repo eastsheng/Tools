f7a5d21a45bb29e62fc761ec0b382a1e8c542b07
eastsheng <eastsheng@hotmail.com>
2020-07-04 18:15:23 +0800

    Update MagicAngle.py

--- MagicAngle/MagicAngle.py
+++ MagicAngle/MagicAngle.py
@@ -1,10 +1,9 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 import seaborn as sns
-from matplotlib.font_manager import FontProperties
 
 def Angle(n,m):
-	Number_data=open('NumberAtom.data','a')
+	Number_data=open('result.data','a')
 	if m==0 and n==0:
 		print("\n跳过m和n同时为0的情况,并设其角度为0\n")
 		angle = 0.000
@@ -19,39 +18,55 @@ def Angle(n,m):
 #Angle(5,1)
 
 def Number_of_Aotm(n,m):
-	Number_data=open('NumberAtom.data','a')
+	Number_data=open('result.data','a')
 	NOA = 32*(m**2+m*n+n**2)
 	print('  (n,m) =','('+str(n)+','+str(m)+')',\
 		'原子总数',NOA,'\n',file=Number_data)
 
-	return print('\n扭转后异质结晶胞：\n',\
-		'(n,m) =','('+str(n)+','+str(m)+')',\
-		'原子总数',NOA,'\n')
+	return NOA
+	# print('\n扭转后异质结晶胞：\n',\
+	# 	'(n,m) =','('+str(n)+','+str(m)+')',\
+	# 	'原子总数',NOA,'\n')
 
 #Number_of_Aotm(5,1)
 
 def Heatmap(n_max,m_max,figure=True):
 	angle_list=[]
+	atomnumber_list=[]
 
 	for i in range(n_max+1):
 		for j in range(m_max+1):
 			angle_list.append(Angle(i,j))
-			Number_of_Aotm(i,j)
+			atomnumber_list.append(Number_of_Aotm(i,j))
 
 	angle_matrix = np.array(angle_list).reshape(n_max+1,m_max+1)
-	
-	plt.figure(figsize=(10,8))
+	atomnumber_matrix = np.array(atomnumber_list).reshape(n_max+1,m_max+1)
+	# print(angle_matrix)
+	# print(atomnumber_matrix)
 
-	sns.set(font_scale=1.5)
-	plt.rc('font',family='Times New Roman',size=18)
-	sns.heatmap(angle_matrix,cmap='jet',linewidth=0.4).invert_yaxis()
+	plt.figure(figsize=(24,8))
+	plt.rc('font',family='Times New Roman',size=12)
+	# sns.set()	
 
-	plt.title('Magic Angle Heatmap (n,m)')
-
+	plt.subplot(121)
+	sns.heatmap(angle_matrix,vmin=0,vmax=60,square=True,cmap='jet',\
+		linewidth=0.4,annot=True,fmt='.3').invert_yaxis()
+	plt.title('Magic Angle Heatmap (n,m)',size=26)
+	plt.xticks(size=22)
+	plt.yticks(size=22)
 	plt.xlabel('n',size=26,)
 	plt.ylabel('m',size=26,)
 
-	plt.savefig('angle_heatmap.tif',dpi=80)
+	plt.subplot(122)
+	sns.heatmap(atomnumber_matrix,vmin=0,vmax=10000,square=True,cmap='jet',\
+		linewidth=0.4,annot=True,fmt='d').invert_yaxis()
+	plt.title('Number of Atoms Heatmap (n,m)',size=26)
+	plt.xticks(size=22)
+	plt.yticks(size=22)	
+	plt.xlabel('n',size=26,)
+	plt.ylabel('m',size=26,)	
+
+	plt.savefig('angle_heatmap.png',dpi=300)
 	if figure==True:
 		plt.show()
 		plt.close()
