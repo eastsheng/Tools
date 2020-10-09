12fb5b49f621e3b082633a402128df939096f5d5
eastsheng <eastsheng@hotmail.com>
2020-10-09 11:05:48 +0800

    Update GULPdispersionMultipath_V2.py

--- FromGULP/GULPdispersionMultipath_V2.py
+++ FromGULP/GULPdispersionMultipath_V2.py
@@ -1,31 +1,14 @@
 # DEAL AND PLOT THE DISPERSION FROM 'GULP' PACKAGE
 import numpy as np 
-import re
 import matplotlib.pyplot as plt
 class GulpDisp(object):
 	"""Gulp Dispersion"""
-	def Readata(self,disp_data,number_atom=96):
+	def Readata(self,disp_data,number_path,number_atom=96):
 		self.disp_data = disp_data
+		self.number_path = number_path		
 		self.number_atom=number_atom
+		print('There are "'+str(self.number_path)+'" Brillouion paths')
 		return 
-
-	def Kpoint(self,number_path,GammatoX=True):
-		self.K = 0
-		self.GammatoX = GammatoX
-		self.number_path = number_path
-		if GammatoX==True:
-			with open(self.disp_data,'r')as disp:
-				for line in disp:
-					if 'Start K point' in line:
-						# print(line)
-						K =re.findall('K point =   (.*?)  ',line)[0]
-						self.K = float(K)
-						print('\nk Point path =','[ 0 ,',str(self.K),']\n')
-					else:
-						pass
-		else:
-			print('There are "'+str(self.number_path)+'" Brillouion paths')
-		return
 
 	def Plot(self,xmin,xmax,ymin,ymax,linewidth,dpi=300,save=True):
 		self.data = np.loadtxt(self.disp_data)
@@ -34,15 +17,10 @@ class GulpDisp(object):
 		# print(len(self.data))
 		len_data = len(self.data)
 		line_number = int(len_data/self.number_atom)
-		if self.GammatoX == True:
-			WaveVector = self.K*(np.unique(self.data[:,0]-1)/(len_data/line_number))
-		else:
-			WaveVector = np.linspace(0,1,line_number)
+		WaveVector = np.linspace(0,1,line_number)
 		Frequency = self.data[:,1].reshape((line_number,self.number_atom))
-		Frequency = Frequency/33.36 # CM-1 >> THz
-		
+		Frequency = Frequency/33.36 # CM-1 >> THz	
 		# print(Frequency.shape)
-
 		# Plot
 		plt.rc('font',family='Times New Roman',size=16)
 		fig,ax = plt.subplots(figsize = (6,8))
@@ -55,7 +33,6 @@ class GulpDisp(object):
 		if save == True:
 			plt.savefig('fig.png',dpi=dpi)
 		plt.show()
-
 		return 
 
 # *************Main Program************* #
@@ -65,13 +42,10 @@ number_atom = 720
 xmin,xmax = [0,1]
 ymin,ymax = [0,2]
 linewidth = 2
-# whether the brillouion zone path from Gamma to X 
-GammatoX=False
 # How many paths?
 number_path  = 2
 
 gulp = GulpDisp()
 # gulp.ReadKpoint('C3NGRAdispersion.disp',number_atom)
-gulp.Readata('20_5x8_S209.disp',number_atom)
-gulp.Kpoint(number_path,GammatoX,)
+gulp.Readata('20_5x8_S209.disp',number_path,number_atom)
 gulp.Plot(xmin,xmax,ymin,ymax,linewidth,dpi=300,save=True)
