8806da63237b12ef546276eb8b44782fdcac8691
eastsheng <eastsheng@hotmail.com>
2020-10-09 22:07:35 +0800

    Update Del_velocity_head_V2.py

--- SED/Del_velocity_head_V2.py
+++ SED/Del_velocity_head_V2.py
@@ -1,18 +1,23 @@
 import numpy as np
-# np.set_printoptions(suppress=True)
+np.set_printoptions(suppress=True)
 # np.set_printoptions(threshold=100000000)  #输出所有的数组，不省略
 
 def Del_Head(velocity_data,dvelocity_data):
 	velocity = np.loadtxt(velocity_data,skiprows=9)
 	# print(velocity)
-	np.savetxt(dvelocity_data,velocity)
-	return print("Done!")	
+	np.savetxt(dvelocity_data,velocity,fmt='%d %.6f %.6f %.6f')
+	return 
 
 
 # Del_Head('velocity.10010.CB.txt','wvelocity.10010.CB.txt')
 
+# Main Program
+print('---Start---')
+
 i = 10010
-while i<=60000:
-	Del_Head('velocity.'+str(i)+'.S8.txt','wvelocity.'+str(i)+'.S8.txt')
+# while i<=60000:
+while i<=10010:
+	Del_Head('velocity.'+str(i)+'.CB.txt','wvelocity.'+str(i)+'.CB.txt')
 	print(str(i))
 	i = i + 10
+print('---Done!---')
