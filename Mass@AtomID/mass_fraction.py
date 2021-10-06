5911155c16b4299d7f6b042760bc3fc39d807a55
eastsheng <eastsheng@hotmail.com>
2021-10-06 14:07:26 +0800

    Update mass_fraction.py

--- Mass@AtomID/mass_fraction.py
+++ Mass@AtomID/mass_fraction.py
@@ -32,19 +32,51 @@ def num2w(Sat_num, aro_num, res_num, asp_num):
 	# print(w)
 	return 
 
-def w2num(Sat_w,aro_w,res_w,asp_w):
+def w2num(error,Sat_w,aro_w,res_w,asp_w):
 	"from the mass fraction of molecules to number"
-	# 暂时没有好的想法。
+	print("Please wait for several minutes...\n...")
+	for i in range(1,100):
+		# print(i)
+		for j in range(1,100):
+			for k in range(1,100):
+				for l in range(1,100):
+					Sat_num = i
+					Sat_totmass = Sat_mass*Sat_num
+					aro_num = j
+					aro_totmass = aro_mass*aro_num
+					res_num = k
+					res_totmass = res_mass*res_num
+					asp_num = l
+					asp_totmass = asp_mass*asp_num
 
-	return
+					tot_mass = Sat_totmass+aro_totmass+res_totmass+asp_totmass
+					sim_Sat_w = (Sat_totmass/tot_mass)*per
+					sim_aro_w = (aro_totmass/tot_mass)*per
+					sim_res_w = (res_totmass/tot_mass)*per
+					sim_asp_w = (asp_totmass/tot_mass)*per
+					e1 = Sat_w-sim_Sat_w
+					e2 = aro_w-sim_aro_w
+					e3 = res_w-sim_res_w
+					e4 = asp_w-sim_asp_w
+									
+					if e1<=error and e2<=error and e3<=error and e4<=error:
+						print("稠油各组分实验含量为（%）：",Sat_w,aro_w,res_w,asp_w)
+						print("稠油各组分模拟含量为（%）：",sim_Sat_w,sim_aro_w,sim_res_w,sim_asp_w)
+						print("稠油各组分模拟分子数为：",Sat_num,aro_num,res_num,asp_num)
+						print("\n")
 
+
+	return print("----------Done----------")
 
 # -------------MAIN PROGRAM-------------#
+
 # input the number of molecules
-num2w(13,10,9,1)
+# num2w(77,58,51,6)
 # num2w(62,46,43,5)
+
 # input the mass fractio of molecules
-# w2num(41.45,22.56,30.78,5.21)
+error = 0.2
+w2num(error,41.45,22.56,30.78,5.21)
 
 
 
